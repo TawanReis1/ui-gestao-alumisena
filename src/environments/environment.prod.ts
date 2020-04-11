d6b1d3d5cd8aafae34e2df42e9c5ca5bb3ffd3f1
Tawan Reis <tawan@rv3ti.com>
2020-04-11 04:05:33 -0300

    fix prod url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gestaoAlumisenaApi: 'https://api-gestao-alumisena.herokuapp.com//api/v1'
+  gestaoAlumisenaApi: 'https://api-gestao-alumisena.herokuapp.com/api/v1'
 };
