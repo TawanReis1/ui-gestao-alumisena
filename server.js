60463bf27401e0925492e4a3ea8f3010b9623e82
Tawan Reis <tawan@rv3ti.com>
2020-04-11 03:37:02 -0300

    fix project informations

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/ui-flyers'));
+app.use(express.static(__dirname + '/dist/ui-gestao-alumisena'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ui-flyers/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ui-gestao-alumisena/index.html'));
 });
 
 // default Heroku PORT
